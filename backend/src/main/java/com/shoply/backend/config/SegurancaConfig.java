5f08b821309d6b14a64ecc5544dd079fb08db9d5
Isabella Roder <isabellaroder631@gmail.com>
2026-09-02 16:06:43 -0300

    feat: adiciona securityfilterchain

--- backend/src/main/java/com/shoply/backend/config/SegurancaConfig.java
+++ backend/src/main/java/com/shoply/backend/config/SegurancaConfig.java
@@ -2,8 +2,13 @@ package com.shoply.backend.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SegurancaConfig {
@@ -11,5 +16,19 @@ public class SegurancaConfig {
     @Bean
     PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+            .csrf(csrf -> csrf.disable())
+            .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        )
+        .authorizeHttpRequests(authorize -> authorize
+            .requestMatchers(HttpMethod.POST, "/api/usuarios").permitAll()
+            .anyRequest().authenticated()
+        )
+        .httpBasic(Customizer.withDefaults())
+        .build();
     }
 }
